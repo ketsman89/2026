63f61db64aac5046c0bda41815a9970076eb35cd
Андрей Кецко <andres@MacBook-Air-Andrej.local>
2026-01-09 15:15:10 +0300

    first

---
+++ first.py
@@ -0,0 +1,8 @@
+a = "Не знаю, как там в Лондоне, я не была. Может там собака - друг человека. А у нас управдом - друг человека!"
+print(len(a))
+print(a[::-1])
+print(a.title())
+print(a.lower())
+print(a.count("нд"))
+print(a.count("ам"))
+print(a.count("о"))
